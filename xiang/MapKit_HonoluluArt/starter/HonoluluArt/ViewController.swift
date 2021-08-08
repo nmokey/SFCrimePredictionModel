583e19341a66f4d7fa819a4c45ec950a2192925d
Xiang Xiao <xxiao23@gmail.com>
2021-08-08 13:47:31 -0700

    added map view kit

--- xiang/MapKit_HonoluluArt/starter/HonoluluArt/ViewController.swift
+++ xiang/MapKit_HonoluluArt/starter/HonoluluArt/ViewController.swift
@@ -27,9 +27,28 @@
 /// THE SOFTWARE.
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+  @IBOutlet private var mapView: MKMapView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    // Set initial location in Honolulu
+    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    mapView.centerToLocation(initialLocation)
+  }
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
   }
 }
