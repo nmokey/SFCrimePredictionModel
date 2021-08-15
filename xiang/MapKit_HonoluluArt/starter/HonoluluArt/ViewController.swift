4e88298d7a06af063c537dd4ceae55216a3d8d4f
Xiang Xiao <xxiao23@gmail.com>
2021-08-15 14:06:47 -0700

    [xiang][mapkit tutorial] annotations with images

--- xiang/MapKit_HonoluluArt/starter/HonoluluArt/ViewController.swift
+++ xiang/MapKit_HonoluluArt/starter/HonoluluArt/ViewController.swift
@@ -32,11 +32,64 @@ import MapKit
 class ViewController: UIViewController {
   @IBOutlet private var mapView: MKMapView!
   
+  private var artworks: [Artwork] = []
+  
+  private func loadInitialData() {
+    // 1
+    guard
+      let fileName = Bundle.main.url(forResource: "PublicArt", withExtension: "geojson"),
+      let artworkData = try? Data(contentsOf: fileName)
+      else {
+        return
+    }
+
+    do {
+      // 2
+      let features = try MKGeoJSONDecoder()
+        .decode(artworkData)
+        .compactMap { $0 as? MKGeoJSONFeature }
+      // 3
+      let validWorks = features.compactMap(Artwork.init)
+      // 4
+      artworks.append(contentsOf: validWorks)
+    } catch {
+      // 5
+      print("Unexpected error: \(error).")
+    }
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Set initial location in Honolulu
     let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
     mapView.centerToLocation(initialLocation)
+    let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
+    let region = MKCoordinateRegion(
+      center: oahuCenter.coordinate,
+      latitudinalMeters: 50000,
+      longitudinalMeters: 60000)
+    mapView.setCameraBoundary(
+      MKMapView.CameraBoundary(coordinateRegion: region),
+      animated: true)
+    
+    let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+    mapView.setCameraZoomRange(zoomRange, animated: true)
+    
+    mapView.delegate = self
+    // Show artwork on map
+    let artwork = Artwork(
+      title: "King David Kalakaua",
+      locationName: "Waikiki Gateway Park",
+      discipline: "Sculpture",
+      coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+    mapView.addAnnotation(artwork)
+    
+    mapView.register(
+      ArtworkView.self,
+      forAnnotationViewWithReuseIdentifier:
+        MKMapViewDefaultAnnotationViewReuseIdentifier)
+    loadInitialData()
+    mapView.addAnnotations(artworks)
   }
 }
 
@@ -52,3 +105,21 @@ private extension MKMapView {
     setRegion(coordinateRegion, animated: true)
   }
 }
+
+extension ViewController: MKMapViewDelegate {
+  func mapView(
+    _ mapView: MKMapView,
+    annotationView view: MKAnnotationView,
+    calloutAccessoryControlTapped control: UIControl
+  ) {
+    guard let artwork = view.annotation as? Artwork else {
+      return
+    }
+
+    let launchOptions = [
+      MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+    ]
+    artwork.mapItem?.openInMaps(launchOptions: launchOptions)
+  }
+}
+
