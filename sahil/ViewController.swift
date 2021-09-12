5ce0e5826513c3a30a06c93bb8ee72f5508b992c
sahilmehta-US <79807412+sahilmehta-US@users.noreply.github.com>
2021-09-12 16:37:06 -0700

    Added location grabber to ViewController

--- sahil/ViewController.swift
+++ sahil/ViewController.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,8 +35,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
   private var artworks: [Artwork] = []
   var locationManager:CLLocationManager!
       var currentLocationStr = "Current location"
-
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Set initial location in Honolulu
@@ -46,13 +45,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     let oahuCenter = CLLocation(latitude: 37.7749, longitude: -122.4194)
     let region = MKCoordinateRegion(
       center: oahuCenter.coordinate,
-      latitudinalMeters: 100000,
-      longitudinalMeters: 100000)
+      latitudinalMeters: 10000,
+      longitudinalMeters: 10000)
     mapView.setCameraBoundary(
       MKMapView.CameraBoundary(coordinateRegion: region),
       animated: true)
     
-    let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 1000000)
+    let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
     mapView.setCameraZoomRange(zoomRange, animated: true)
     
     mapView.delegate = self
@@ -62,59 +61,33 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
       forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
     
     loadInitialData()
+    getUserLocation()
     mapView.addAnnotations(artworks)
   }
-  override func viewDidAppear(_ animated: Bool) {
-          determineCurrentLocation()
-      }
+  
+  func getUserLocation() {
+    locationManager = CLLocationManager()
+    locationManager?.requestAlwaysAuthorization()
+    locationManager?.startUpdatingLocation()
+    locationManager?.delegate = self
+  }
+
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
           let mUserLocation:CLLocation = locations[0] as CLLocation
 
           let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
           let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-    let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
-        mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
-        mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
-        mapView.addAnnotation(mkAnnotation)
-
-
-          mapView.setRegion(mRegion, animated: true)
-      }
-  func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
-      let geoCoder = CLGeocoder()
-      let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
-
-      geoCoder.reverseGeocodeLocation(location) {
-          (placemarks, error) -> Void in
-
-          if let mPlacemark = placemarks{
-              if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
-                  if let Name = dict["Name"] as? String{
-                      if let City = dict["City"] as? String{
-                          self.currentLocationStr = Name + ", " + City
-                      }
-                  }
-              }
-          }
-      }
-      return currentLocationStr
+              mapView.setRegion(mRegion, animated: true)
+    if let location = locations.last {
+      print("Lat : \(location.coordinate.latitude) \nLng : \(location.coordinate.longitude)")
+    }
   }
-
+  
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
           print("Error - locationManager: \(error.localizedDescription)")
       }
   //MARK:- Intance Methods
 
-  func determineCurrentLocation() {
-      locationManager = CLLocationManager()
-      locationManager.delegate = self
-      locationManager.desiredAccuracy = kCLLocationAccuracyBest
-      locationManager.requestAlwaysAuthorization()
-
-      if CLLocationManager.locationServicesEnabled() {
-          locationManager.startUpdatingLocation()
-      }
-  }
   
   private func loadInitialData() {
     // 1
@@ -165,4 +138,3 @@ extension ViewController {
     artwork.mapItem?.openInMaps(launchOptions: launchOptions)
   }
 }
-
