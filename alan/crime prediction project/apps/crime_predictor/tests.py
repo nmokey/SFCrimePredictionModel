0ee0c01913f7aea3487673fd4a18dfb26d7e50bc
Alan Xiao <alanxiao211@gmail.com>
2022-08-21 11:29:00 -0700

    updated backend

--- alan/crime prediction project/apps/crime_predictor/tests.py
+++ alan/crime prediction project/apps/crime_predictor/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from models import CrimePredictionLog
 
 # Create your tests here.
+class CrimePredictionTest(TestCase):
+	def test_GETRequest(self):
+		res = self.client.get('/crime/predict')
+		self.assertEqual(res.status_code, 200)
+
+class CrimePredictionDatabaseLogsTest(TestCase):
+	def test_save_log(self):
+		dummyLog = CrimePredictionLog()
+		dummyLog.save()
+		self.assertTrue(CrimePredictionLog.objects.filter().exists())
--- alan/crime prediction project/apps/crime_predictor/views.py
+++ alan/crime prediction project/apps/crime_predictor/views.py
@@ -28,11 +28,39 @@ def feature_engineering(data):
         
     return data
 
+def checkInputs(X):
+	features = ['Dates', 'PdDistrict', 'Longitude', 'Latitude', 'DayOfWeek', 'Address']
+	types = [str, str, float, float, str, str]
+	DOW_CHOICES = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+	PDD_CHOICES = ['TENDERLOIN', 'RICHMOND', 'NORTHERN', 'SOUTHERN', 'CENTRAL', 'BAYVIEW', 'INGLESIDE',
+	'MISSION', 'PARK', 'TARAVAL']
+
+	if set(X.keys()) != set(features):
+		return False 
+
+	for i, f in enumerate(features):  
+		if type(X[f]) != types[i]:
+			return False 
+	
+		if f == 'DayOfWeek' and X[f].strip().upper() not in DOW_CHOICES:
+			# print("DOW Error")
+			return False 
+
+		if f == 'PdDistrict' and X[f].strip().upper() not in PDD_CHOICES: 
+			# print("PdDistrict Error")
+			return False 
+	
+	return True 
+	
+	
+
 def process_data(X):
 	le1 = LabelEncoder()
 	le1.classes_ = np.load('./research/LabelEncoders/le1_classes.npy', allow_pickle = True)
 	
-	X['Dates'] = pd.to_datetime(X['Dates'])
+	X['Dates'] = pd.to_datetime(X['Dates'], errors = 'raise')
+	print(X['Dates'])
+	
 	X = feature_engineering(X)
 	X['PdDistrict'] = le1.transform(X['PdDistrict'])
 	X.rename(columns = {'Longitude': 'X', 'Latitude': 'Y'}, inplace = True)
@@ -48,25 +76,44 @@ def predict_GET(req):
 def predict_POST(req):
 	try: 
 		body_unicode = req.body.decode('utf-8')
-		body = json.loads(body_unicode)['Features']
+		body = json.loads(body_unicode)
+		print(body)
+		body = body['Features']
+		assert checkInputs(body)
 		X = pd.DataFrame([body])
 		X = process_data(X)
-		# print(X)
 
+	#Error handling for data preprocessing
 	except (KeyError, CrimePredictionLog.DoesNotExist):
 		return JsonResponse({'Message': 'Prediction not ok :(', 'Result': 'KeyError: Please enter input data'}, 
 		safe = False)
 
+	except(AssertionError):
+		print("Invalid inputs, make sure inputs are all of the proper type and values.")
+		return JsonResponse({'Message': 'Invalid inputs, try again.', 
+		'Result': 'Invalid inputs, make sure inptus are all of the proper type and values.'}, safe = False)
+
+	except(pd.errors.ParserError):
+		return JsonResponse({'Message': 'ParserError - parser error encountered, prediction input preprocessing failed.', 
+		'Result': 'ParserError - prediction input invalid, check if datetime given was a valid datetime string.'}, safe = False)
+
+	except(ValueError):
+		return JsonResponse({'Message': 'ValueError - datetime parsing during prediction input preprocessing failed.', 
+		'Result': 'ValueError - prediction input invalid, check if datetime given was a valid datetime string.'}, safe = False)
+
 	else:
 		model = pickle.load(open('./research/models/model', 'rb'))
 		
-		new_log = CrimePredictionLog(
-			Date = timezone.make_aware(dateparse.parse_datetime(body['Dates'])),
-			Latitude = body['Latitude'],
-			Longitude = body['Longitude'],
-			PdDistrict = body['PdDistrict'],
-			DayOfWeek = body['DayOfWeek'].upper()
-		)
+		try:
+			new_log = CrimePredictionLog(
+				Date = timezone.make_aware(dateparse.parse_datetime(body['Dates'])),
+				Latitude = body['Latitude'],
+				Longitude = body['Longitude'],
+				PdDistrict = body['PdDistrict'],
+				DayOfWeek = body['DayOfWeek'].upper()
+			)
+		except(ValueError) as ve:
+			print(ve)
 		# print(new_log.DayOfWeek)
 		# print(new_log.PdDistrict)
 		new_log.save()
