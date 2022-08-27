b439b67b52a3825c855cc1dccfcf0fe48b2da3d6
Xiang Xiao <xxiao23@gmail.com>
2022-08-27 12:49:32 -0700

    [ios] show top crime on a map annotation for the current location

---
+++ xiang/sfcrimeios/sfcrimeios/CrimePredictionMapAnnotation.swift
@@ -0,0 +1,40 @@
+//
+//  CrimePredictionMapAnnotation.swift
+//  sfcrimeios
+//
+//  Created by Xiang Xiao on 8/27/22.
+//  Copyright © 2022 Xiang Xiao. All rights reserved.
+//
+
+import Foundation
+
+import MapKit
+
+class CrimePredictionMapAnnotation: NSObject, MKAnnotation {
+  let title: String?
+  let locationName: String?
+  var coordinate: CLLocationCoordinate2D
+    
+  let crimes: [String]
+  let probabilities: [Double]
+
+  init(
+    title: String?,
+    locationName: String?,
+    coordinate: CLLocationCoordinate2D,
+    crimes: [String],
+    probabilities: [Double]
+  ) {
+    self.title = title
+    self.locationName = locationName
+    self.coordinate = coordinate
+    self.crimes = crimes
+    self.probabilities = probabilities
+
+    super.init()
+  }
+
+  var subtitle: String? {
+      return crimes[0] + ":" + String(format: "%2.0f", probabilities[0]*100) + "%"
+  }
+}
--- xiang/sfcrimeios/sfcrimeios/ViewController.swift
+++ xiang/sfcrimeios/sfcrimeios/ViewController.swift
@@ -18,11 +18,6 @@ class ViewController: UIViewController {
   var locationManager:CLLocationManager!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    let austin = MKPointAnnotation()
-    austin.coordinate = CLLocationCoordinate2DMake(30.25, -97.75)
-    austin.title = "Austin"
-    mapView.addAnnotation(austin)
     locationManager = CLLocationManager.init()
     locationManager.delegate = self
     locationManager.requestWhenInUseAuthorization()
@@ -32,6 +27,49 @@ class ViewController: UIViewController {
     }
   }
 
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
+
+import Contacts
+
+extension Formatter {
+    static let mailingAddress: CNPostalAddressFormatter = {
+        let formatter = CNPostalAddressFormatter()
+        formatter.style = .mailingAddress
+        return formatter
+    }()
+}
+
+extension CLPlacemark {
+    var mailingAddress: String? {
+        postalAddress?.mailingAddress
+    }
+}
+
+extension CNPostalAddress {
+    var mailingAddress: String {
+        Formatter.mailingAddress.string(from: self)
+    }
+}
+
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).capitalized
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
@@ -54,43 +92,119 @@ extension ViewController: CLLocationManagerDelegate {
     }
   }
   
+  func geocode(latitude: Double, longitude: Double, completion: @escaping (_ placemark: [CLPlacemark]?, _ error: Error?) -> Void)  {
+      CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: latitude, longitude: longitude), completionHandler: completion)
+  }
+
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
     print("locations = \(locValue.latitude) \(locValue.longitude)")
+    let currentLocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
+    mapView.centerToLocation(currentLocation)
 
     struct CrimePredictFeatures: Encodable {
-      let Latitude: Float32
-      let Longitude: Float32
+      let Latitude: Double
+      let Longitude: Double
       let Address: String
       let Dates: String
-      let PdDistrict: String
       let DayOfWeek: String
+      let PdDistrict: String
     }
-    struct CrimePredictPostData: Encodable {
-      let Features: CrimePredictFeatures
+    class CrimePredictPostData: Encodable {
+      var Features: CrimePredictFeatures
+      
+      init(Features: CrimePredictFeatures) {
+        self.Features = Features
+      }
+      
+      init() {
+        self.Features = CrimePredictFeatures(
+          Latitude: 0,
+          Longitude: 0,
+          Address: "",
+          Dates: "",
+          DayOfWeek: "",
+          PdDistrict: ""
+        )
+      }
     }
-    let postData = CrimePredictPostData(Features: CrimePredictFeatures(Latitude: 37.7873589, Longitude: -122.408227, Address: "42042 Indigo Dr", Dates: "2022-08-20 20:15:19", PdDistrict: "SOUTHERN", DayOfWeek: "Wednesday"))
-    let encoder = JSONEncoder()
-    let parameterEncoder = JSONParameterEncoder(encoder: encoder)
+    geocode(latitude: locValue.latitude, longitude: locValue.longitude, completion: {
+      placemark, error in
+        if let error = error as? CLError {
+          print("CLError:", error)
+          return
+        } else if let placemark = placemark?.first {
+          //  update UI here
+          print("name:", placemark.name ?? "unknown")
+          
+          print("address1:", placemark.thoroughfare ?? "unknown")
+          print("address2:", placemark.subThoroughfare ?? "unknown")
+          print("neighborhood:", placemark.subLocality ?? "unknown")
+          print("city:", placemark.locality ?? "unknown")
+          
+          print("state:", placemark.administrativeArea ?? "unknown")
+          print("subAdministrativeArea:", placemark.subAdministrativeArea ?? "unknown")
+          print("zip code:", placemark.postalCode ?? "unknown")
+          print("country:", placemark.country ?? "unknown", terminator: "\n\n")
+          
+          print("isoCountryCode:", placemark.isoCountryCode ?? "unknown")
+          print("region identifier:", placemark.region?.identifier ?? "unknown")
+          
+          print("timezone:", placemark.timeZone ?? "unknown", terminator:"\n\n")
+          // Mailind Address
+          print(placemark.mailingAddress ?? "unknown")
+          // Get current date time.
+          let currentDateTime = Date()
+          let RFC3339DateFormatter = DateFormatter()
+          RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+          RFC3339DateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+          RFC3339DateFormatter.timeZone = TimeZone.current
+          // Build crime prediction features.
+          let features = CrimePredictFeatures(
+            Latitude: locValue.latitude,
+            Longitude: locValue.longitude,
+            Address: placemark.mailingAddress ?? "unknown",
+            Dates: RFC3339DateFormatter.string(from: currentDateTime),
+            DayOfWeek: currentDateTime.dayOfWeek()!,
+            PdDistrict: "SOUTHERN"  // "SOUTHERN" is a dummy value here. Just to make the server not return error.
+          )
+          print("Reverse geocoding finished!\n")
+          let postData = CrimePredictPostData(Features: features)
+          debugPrint(postData.Features)
+          
+          let encoder = JSONEncoder()
+          let parameterEncoder = JSONParameterEncoder(encoder: encoder)
 
-    struct CrimeProb: Decodable {
-      let Crime: String
-      let Prob: Float64
-    }
-    struct CrimePredictResult: Decodable {
-      let Message: String
-      let Result: [CrimeProb]
-    }
-    AF.request("http://127.0.0.1:8000/crime/predict/",
-               method: .post,
-               parameters: postData,
-               encoder: parameterEncoder)
-    .responseDecodable(of: CrimePredictResult.self) { (response) in
-      debugPrint(response)
-      guard let predicts = response.value else { return }
-      print(predicts.Message)
-      print(predicts.Result.sorted{$0.Prob > $1.Prob}[0])
-    }
+          struct CrimeProb: Decodable {
+            let Crime: String
+            let Prob: Float64
+          }
+          struct CrimePredictResult: Decodable {
+            let Message: String
+            let Result: [CrimeProb]
+          }
+          AF.request("http://127.0.0.1:8000/crime/predict/",
+                     method: .post,
+                     parameters: postData,
+                     encoder: parameterEncoder)
+          .responseDecodable(of: CrimePredictResult.self) { (response) in
+            debugPrint(response)
+            guard let predicts = response.value else { return }
+            print(predicts.Message)
+            let topPrediction = predicts.Result.sorted{$0.Prob > $1.Prob}[0]
+            print(topPrediction)
+            
+            // Add map annotation for the top crime.
+            let cpAnnotation = CrimePredictionMapAnnotation(
+              title: placemark.subLocality ?? "unknown",
+              locationName: features.Address,
+              coordinate: locValue,
+              crimes: [topPrediction.Crime],
+              probabilities: [topPrediction.Prob])
+            self.mapView.addAnnotation(cpAnnotation)
+          }
+        }
+    })
   }
   
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
